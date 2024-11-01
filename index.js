8e0ca8fe615a571145c9d1c9e98097bd5d8d4c82
Marcelo Alejandro Bravo Sugo <marcelo.bravo@fing.edu.uy>
2024-11-01 00:59:26 -0300

    Agrego tests

--- index.js
+++ index.js
@@ -19,3 +19,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 });
+
+export default app;
---
+++ test/test.js
@@ -0,0 +1,61 @@
+import request from 'supertest';
+import app from '../index.js';
+
+
+describe('GET /api/consulta/:cedula/:pagina/:limite', function() {
+	it('Consultar historial medico paginado', function(done) {
+	  request(app)
+		.get('/api/consulta/46810370/1/1')
+		.expect(200, done);
+	});
+  });
+
+  
+  describe('POST /api/paciente', function() {
+	it('Agregar un paciente', function(done) {
+      let paciente = {
+        "cedula": "16449135",
+        "nombre": "Silvia",
+        "apellido": "Sugo",
+        "fechaNacimiento": "1959-07-12",
+        "sexo": "Femenino"
+    }  
+	  request(app)
+		.post('/api/paciente')
+        .send(paciente)
+		.expect(401, done);
+	});
+  });
+  
+  describe('POST /api/registro-medico', function() {
+	it('Agregar un registro medico', function(done) {
+      let registroMedico = {
+        "fechaAlta": "2024-10-01 14:00:00",
+        "tipo": "Examen",
+        "diagnostico": "Analisis de Sangre",
+        "medico": "Miguel Merentiel",
+        "institucion": "Casmu",
+        "cedula_paciente": "16449135"
+    }  
+	  request(app)
+		.post('/api/registro-medico')
+        .send(registroMedico)
+		.expect(200, done);
+	});
+  });
+
+
+  describe('GET /api/busqueda', function() {
+	it('Obtener registros medicos segun filtros de busqueda', function(done) {
+      let criterioBusqueda = {
+        "tipo":"", 
+        "diagnostico":"", 
+        "medico":"", 
+        "institucion":"Casmu"
+    }  
+	  request(app)
+		.get('/api/busqueda')
+        .send(criterioBusqueda)
+		.expect(200, done);
+	});
+  });
